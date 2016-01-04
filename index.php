78ac474e4f65f576d14cec7ef85105cbea965137
sth910227 <bill93518@gmail.com>
2016-01-04 09:08:56 +0800

    升泰

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html><!--TEST-->
+<html lang="en">
+<head>
+	<title>NaRoller</title>
+    <meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--
+OneTel Template
+http://www.templatemo.com/tm-468-onetel
+-->
+	<!-- stylesheet css -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/nivo-lightbox.css">
+	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+	<link rel="stylesheet" href="css/templatemo-style.css">
+	<link rel="stylesheet" href="css/main.css">
+	<!-- google web font css -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+<!-- navigation -->
+
+	<div class="container">
+		<div class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brand"><font color="#ff1493">NaRoller</font>
+			</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<!--li><a href="index.html" class="active">首頁</a></li -->
+				<li><a href="about.html">關於我們</a></li>
+				<li><a href="about.html">最新消息</a></li>
+				<li><a href="portfolio.html">產品</a></li>
+				<li><a href="portfolio.html">技術</a></li>
+				<li><a href="contact.html">聯繫我們</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<!-- home section -->
+<div id="home">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-sm-3"></div>
+			<div class="col-md-7 col-sm-9">
+				<h3>歡迎來到</h3>
+				<h1>奈美科技股份有限公司</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- divider section -->
+<div class="divider">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-4 col-sm-6">
+				<div class="divider-wrapper divider-one">
+					<img src="pic\3.jpg" height="80" width="200">
+					<h2>結構力學</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 col-sm-6">
+				<div class="divider-wrapper divider-two">
+					<img src="pic\4.jpg" height="80" width="200">
+					<h2>電磁場</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 col-sm-12">
+				<div class="divider-wrapper divider-three">
+					<img src="pic\5.jpg" height="80" width="200">
+					<h2>系統多物理場</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+				</div>
+			</div>
+
+		</div><!-- End row -->
+	</div><!-- End container-->
+</div>
+<!-- End divider section -->
+<!-- about section -->
+<div id="about">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12">
+				<h2>News</h2><!--span class="fa fa-newspaper-o fa-2x"></span-->
+			</div>
+		</div>
+
+		<div class="row">
+
+			<div class="col-md-6 col-sm-12" id="newsImgArea">
+				<img src="pic\3.jpg" class="img-responsive" alt="about img">
+			</div>
+
+			<div class="col-md-6 col-sm-12 about-des">
+				<h3>Startup Business</h3>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet.</p>
+				<a href="about.html" class="btn btn-default" id="newsButton">LEARN MORE</a>
+			</div>
+
+		</div><!-- End row -->
+
+	</div>
+</div>
+
+<!-- portfolio section -->
+<div id="portfolio">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12">
+				<h2>Product</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
+			</div>
+         </div>
+
+    <div class="row mt30">
+
+			<div class="col-md-4 col-sm-4 col-xs-6">
+				<a href="images/portfolio-img1.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img1.jpg" alt="portfolio img"></a>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-6">
+				<a href="images/portfolio-img2.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img2.jpg" alt="portfolio img"></a>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-6">
+				<a href="images/portfolio-img3.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img3.jpg" alt="portfolio img"></a>
+			</div>
+
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<a href="images/portfolio-img4.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img4.jpg" alt="portfolio img"></a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<a href="images/portfolio-img5.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img5.jpg" alt="portfolio img"></a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<a href="images/portfolio-img6.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img6.jpg" alt="portfolio img"></a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-xs-12">
+				<a href="images/portfolio-img7.jpg" data-lightbox-gallery="portfolio-gallery"><img src="images/portfolio-img7.jpg" alt="portfolio img"></a>
+			</div>
+			<div class="col-md-12 col-sm-12">
+				<a rel="nofollow" href="http://www.templatemo.com/tag/darkgray" class="btn btn-default">View More</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- footer section -->
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-5 col-sm-4">
+				<img src="images/logo.png" class="img-responsive" alt="logo">
+				<p>Nano is the future
+
+				<p><i class="fa fa-phone"></i> 000-000-0000</p>
+				<p><i class="fa fa-envelope-o"></i> info@naroller.com</p>
+              <p><i class="fa fa-globe"></i> www.naroller.com</p>
+			</div>
+
+			<div class="col-md-3 col-sm-4">
+				<h3>Menu</h3>
+				<p><a href="#">About</a></p>
+				<p><a href="#">News</a></p>
+				<p><a href="#">Products</a></p>
+				<p><a href="#">Technology</a></p>
+				<p><a href="#">Contact</a></p>
+			</div>
+
+			<div class="col-md-4 col-sm-4 newsletter">
+				<h3>Map</h3>
+				<p>Place Google Map here.</p>
+<!--
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismo.</p>
+				<div class="input-group">
+           	    	<form action="#" method="post">
+                        <input name="email" type="email" placeholder="Enter your email" class="form-control" autorequired>
+                    	<button type="submit" name="submit" class="btn email">Submit</button>
+                  	</form>
+				 </div> -->
+			</div>
+
+		</div>
+	</div>
+</footer>
+
+<!-- copyright section -->
+<div class="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<p>Copyright © 2015 NaRoller</p>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<ul class="social-icons">
+					<li><a href="#" class="fa fa-facebook"></a></li>
+					<li><a href="#" class="fa fa-twitter"></a></li>
+					<li><a href="#" class="fa fa-dribbble"></a></li>
+					<li><a href="#" class="fa fa-pinterest"></a></li>
+					<li><a href="#" class="fa fa-behance"></a></li>
+					<li><a href="#" class="fa fa-envelope-o"></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- javascript js -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/nivo-lightbox.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
