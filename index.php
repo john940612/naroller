2e09b48b389aa5a115079c138020d0fe96a5a4c0
29776457 <a048291106@gmail.com>
2016-01-07 22:47:00 +0800

    新增所有產品分頁，並刪除過多的產品分頁連結

--- index.php
+++ index.php
@@ -43,34 +43,25 @@ http://www.templatemo.com/tm-468-onetel
 			<ul class= "nav navbar-nav navbar-right drop-down-menu">
 				<!--li><a href="index.html" class="active">首頁</a></li -->
 				<li><a href="about.html">關於我們</a></li>
-				<li><a href="about.html">最新消息</a></li>
+				<li><a href="news.html">最新消息</a></li>
 				<li><a href="portfolio.html">產品</a>
 					<ul>
-						<li><a href="">Mini Sedan</a>
+						<li><a href="mini.html">Mini Sedan</a>
 							<ul>
-								<li><a href="">Hyundai Accent</a></li>
-								<li><a href="">Proton saga</a></li>
-								<li><a href="">Toyota Yaris/VItz</a></li>
+								<li><a href="Hyundai Accent.html">Hyundai Accent</a></li>
+								<li><a href="Proton saga.html">Proton saga</a></li>
 							</ul>
 						</li>
-						<li><a href="">Compact Sedan</a>
+						<li><a href="compact.html">Compact Sedan</a>
 							<ul>
-								<li><a href="">Ford Focus</a></li>
-								<li><a href="">Proton Gen-2</a></li>
-								<li><a href="">Honda Civic</a></li>
-								<li><a href="">Mitsubishi Lancer</a></li>
-								<li><a href="">Toyota Corolla Altis</a></li>
-								<li><a href="">Nissan Tiida 4-Door</a></li>
+								<li><a href="Ford Focus.html">Ford Focus</a></li>
+								<li><a href="Proton Gen-2.html">Proton Gen-2</a></li>
 							</ul>
 						</li>
-						<li><a href="">Mid-size Sedan</a>
+						<li><a href="mid.html">Mid-size Sedan</a>
 							<ul>
-								<li><a href="">Ford Mondeo</a></li>
-								<li><a href="">Honda Accord</a></li>
-								<li><a href="">Peugeot 407</a></li>
-								<li><a href="">Nissan Altima</a></li>
-								<li><a href="">Toyota Camry</a></li>
-								<li><a href="">mazda 6</a></li>
+								<li><a href="Ford Mondeo.html">Ford Mondeo</a></li>
+								<li><a href="Honda Accord.html">Honda Accord</a></li>
 							</ul>
 						</li>
 					</ul>
