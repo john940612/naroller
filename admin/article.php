65101acd13f66cf82e6a950a37988153dc8c59e9
sth910227 <bill93518@gmail.com>
2016-01-07 18:47:10 +0800

    article modify

--- admin/article.php
+++ admin/article.php
@@ -9,30 +9,12 @@
 	$aText = addslashes($_POST['aText']);
 	$aSummary = addslashes($_POST['aSummary']);
 	$aDateTime = date("Y-m-d H:i:s");
-<<<<<<< HEAD
-/*
-	$aTitle = "test";
-	$aSecondTitle = "test";
-	$aText = "test";
-	$aSummary = "test";
-	$aDateTime = date("Y-m-d / H:i:s");
-*/
 
-	echo $aTitle;
-	echo $aSecondTitle;
-	echo $aText;
-	echo $aSummary;
-	echo $aDateTime;
-
-=======
-	
->>>>>>> e7b057fd026fd602517553fc23be6d3612914152
 	try
 	{
 		
 		$stmt = $dbh->prepare("INSERT INTO article(aTitle, aSecondTitle, aText, aSummary, aDateTime)
 			VALUES(:aTitle, :aSecondTitle, :aText, :aSummary, :aDateTime)");
-<<<<<<< HEAD
 
 		//$stmt->bindparam(":aId", $aId);
 		$stmt->bindparam(":aTitle", $aTitle);
@@ -42,18 +24,6 @@
 		$stmt->bindparam(":aDateTime", $aDateTime);
 
 		$stmt->execute();
-		echo "1";
-=======
-	
-		$stmt->bindParam(':aTitle', $aTitle);
-		$stmt->bindParam(':aSecondTitle', $aSecondTitle);
-		$stmt->bindParam(':aText', $aText);
-		$stmt->bindParam(':aSummary', $aSummary);
-		$stmt->bindParam(':aDateTime', $aDateTime);
-		if(!$stmt->execute()){
-			var_dump($stmt->errorInfo());
-		}
->>>>>>> e7b057fd026fd602517553fc23be6d3612914152
 
 	}
 
@@ -62,13 +32,5 @@
 	{
 		echo $e->getMessage();
 	}
-/*
-	if(!$dbh->queryExec($stmt))
-	{
-		echo 'fail';
-	}*/
-
-
-	//header("location: articleManagementExample.php");
 
 ?>
