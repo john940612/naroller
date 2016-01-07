d75b4e12edb3a5bdf27130924d50ab9cab690b32
RandWu <a84888888@gmail.com>
2016-01-07 15:40:10 +0800

    可以新增文章了

--- admin/article.php
+++ admin/article.php
@@ -7,21 +7,22 @@
 	$aSecondTitle = addslashes($_POST['aSecondTitle']);
 	$aText = addslashes($_POST['aText']);
 	$aSummary = addslashes($_POST['aSummary']);
-	$aDateTime = date("Y-m-d / H:i:s");
-
+	$aDateTime = date("Y-m-d H:i:s");
+	
 	try
 	{
 
 		$stmt = $dbh->prepare("INSERT INTO article(aTitle, aSecondTitle, aText, aSummary, aDateTime)
-			VALUES(:aTitle, :aSecondTitle, :aText, :aSummary, :aDateTime)" );
+			VALUES(:aTitle, :aSecondTitle, :aText, :aSummary, :aDateTime)");
 	
-		$stmt->bindparam(":aTitle", $aTitle);
-		$stmt->bindparam(":aSecondTitle", $aSecondTitle);
-		$stmt->bindparam(":aText", $aText);
-		$stmt->bindparam(":aSummary", $aSummary);
-		$stmt->bindparam(":aDateTime", $aDateTime);
-
-		$stmt->execute();
+		$stmt->bindParam(':aTitle', $aTitle);
+		$stmt->bindParam(':aSecondTitle', $aSecondTitle);
+		$stmt->bindParam(':aText', $aText);
+		$stmt->bindParam(':aSummary', $aSummary);
+		$stmt->bindParam(':aDateTime', $aDateTime);
+		if(!$stmt->execute()){
+			var_dump($stmt->errorInfo());
+		}
 
 	}
 	catch(PDOException $e)
