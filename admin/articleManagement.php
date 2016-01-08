c8f7b3f3fc5da327dd572ac01c6fd97c3f08179c
29776457 <a048291106@gmail.com>
2016-01-08 08:13:12 +0800

    modify link

--- admin/articleManagement.php
+++ admin/articleManagement.php
@@ -36,7 +36,7 @@ http://www.templatemo.com/tm-468-onetel
 				<span class="icon icon-bar"></span>
 				<span class="icon icon-bar"></span>
 			</button>
-			<a href="index.html" class="navbar-brand">
+			<a href="../index.php" class="navbar-brand">
                 <img id="logo" src="../images/NaRollerLogo-w500.png" />
             </a>
 		</div>
