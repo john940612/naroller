cb8dbf8279cfac177d25f6d1a0faee64c33653a6
john940612 <john940612@gmail.com>
2016-01-08 08:38:48 +0800

    改鏈結

--- news_list.php
+++ news_list.php
@@ -1,53 +1,16 @@
-<?php
-	include("connect_db.php");
-	//$flag = 1;
-	//include("display_paging.php");
-	$catch = "SELECT * FROM article";
-	echo "<table border='0'>";
-	echo "<tr>";
-		echo "<th><center>標題</center></th>";
-		//echo "<th>時間</th>";
-		echo "<th width=''>副標題</th>";
-		//echo "<th>副標</th>";
-		//echo "<th colspan='2'>文章</th>";
-		echo "<th colspan='2'>內容</th>";
-	echo "</tr>";
-	foreach($dbh->query($catch) as $row)
-	{
-		/*
-		if($flag)
-		{
-			
-			$flag = 0;
-		}   */
-		echo "<tr>";
-			echo "<td width=''>";
-				print_r ($row['aTitle']);
-				echo '<input name="aTitle" type="hidden" class="form-control" id="name" value="'.$row['aTitle'].'"/>';
-			echo "</td>";       
-			//echo "<td>";
-				//print_r ($row['aDateTime']);
-			//echo "</td>";
-			echo "<td width=''>";
-				print_r ($row['aSecondTitle']);
-				echo '<input name="aTitle" type="hidden" class="form-control" id="name" value="'.$row['aSecondTitle'].'"/>';
-			echo "</td>";
-			//echo "<td>";
-				//print_r ($row['aSecondTitle']);
-				//echo '<input name="aSecondTitle" type="hidden" class="form-control" id="email" value="'.$row['aSecondTitle'].'"/>';
-			//echo "</td>";
-			//echo "<td colspan='2'>";
-				//print_r ($row['aText']);
-				//echo '<input name="aText" type="hidden" class="form-control" id="email"  value="'.$row['aText'].'"/>';
-			//echo "</td>";
-			echo "<td colspan='2' width='30%'>";
-				print_r ($row['aText']);
-				echo '<input name="aText" type="hidden" class="form-control" id="email"value="'.$row['Text'].'" />';
-			echo "</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
-
-
-
-?>
+		<?php
+			$db_server = "140.117.194.50";
+			//資料庫名稱
+			$db_name = "sad";
+			//資料庫管理者帳號
+			$db_user = "sad";
+			//資料庫管理者密碼
+			$db_passwd = "a0123456789";
+			$sql1 = "SELECT * FROM article";
+        	$result = mysql_query($sql1);  
+			while($row = mysql_fetch_row($result))
+	        	{	        		
+		             echo "<center><table bgcolor=#FFDC35 ><tr><td width='20%'>Topic: $row[1] </td> <td width='20%'>Second Title：$row[2]</td> " . 
+		             "<td width='20%'>Summary：$row[4]</td></tr> <tr><td colspan='3'>Content：$row[3]</td></tr></table><br>";
+	        	}
+		?>
