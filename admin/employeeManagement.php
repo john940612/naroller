c6d18fbddc8399185f0cf6e79a3464f841490611
29776457 <a048291106@gmail.com>
2016-01-10 17:26:43 +0800

    ...

--- admin/employeeManagement.php
+++ admin/employeeManagement.php
@@ -56,7 +56,7 @@ http://www.templatemo.com/tm-468-onetel
                 <li><a href="product_list.php">產品管理</a></li>
 				<li><a href="mediaManagement.php">媒體管理</a></li>
 				<li><a class="activePage" href="employeeManagement.php">員工管理</a></li>
-				<li><a href="#">登出</a></li>
+				<li><a href="logout.php">登出</a></li>
 			</ul>
 		</div>
 	</div>
