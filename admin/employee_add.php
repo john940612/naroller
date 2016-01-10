fad9ab6e76fd18d7ccbdbb285cdbe51280468460
RandWu <a84888888@gmail.com>
2016-01-10 18:37:13 +0800

    employee add

--- admin/employee_add.php
+++ admin/employee_add.php
@@ -4,22 +4,24 @@
 		$eId = addslashes($_POST['eId']);
 		$eName = addslashes($_POST['eName']);
 		$ePwd = addslashes($_POST['ePwd']);
-		$ePerm = addslashes($_POST['ePerm']);
+		//$ePerm = addslashes($_POST['ePerm']);
 		
 	
 		try
 		{
 			
-			$stmt = $dbh->prepare("INSERT INTO employee(eId,ePName,ePwd,ePerm)
-				VALUES(:eId, :eName, :ePwd,'general')");
+			$stmt = $dbh->prepare("INSERT INTO employee(eId,eName,ePwd)
+				VALUES(:eId, :eName, :ePwd)");
 	
 			//$stmt->bindparam(":aId", $aId);
 			$stmt->bindparam(":eId", $eId);
 			$stmt->bindparam(":eName", $eName);
 			$stmt->bindparam(":ePwd", $ePwd);
-			$stmt->bindparam("general", $ePerm);
-	
-			$stmt->execute();
+			//$stmt->bindparam("general", $ePerm);
+			if(!$stmt->execute()){
+				var_dump($stmt->errorInfo());
+			}	
+			//$stmt->execute();
 	
 		}
 	
